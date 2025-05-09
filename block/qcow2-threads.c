d8660276b8a5773128c0990c739773c17c110653
nixxou <45721836+nixxou@users.noreply.github.com>
2025-05-09 15:40:46 +0200

    Update qcow2-threads.c

--- block/qcow2-threads.c
+++ block/qcow2-threads.c
@@ -235,6 +235,7 @@ static ssize_t qcow2_zstd_compress(void *dest, size_t dest_size,
 
     /* make sure that zstd didn't overflow the dest buffer */
     assert(output.pos <= dest_size);  
+    fprintf(stderr, "ZSTD: src=%zu out=%zu\n", src_size, output.pos);
     ret = -ENOMEM;
     goto out;
 
